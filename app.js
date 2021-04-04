bcdaac5111f58a6d8ff1e00469d6ec72bfd1bdc1
Sena <github@sounoob.com.br>
2021-04-04 19:04:06 -0300

    Remove unused dep

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const routerMovie = require('./routes/movie');
 
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
