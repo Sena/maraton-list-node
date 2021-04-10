455e2c5ec8029169ade66b727774b6da1b9d9df0
Sena <github@sounoob.com.br>
2021-04-10 12:58:11 -0300

    Create user/login router

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,24 +1,25 @@
-const bcrypt = require('bcrypt');
-const mongoose = require('../config/mongoose');
-const {UserSchema} = require('../model/user-model');
+const bcrypt = require('bcrypt')
+const mongoose = require('../config/mongoose')
+const jsonwebtoken = require("jsonwebtoken")
+const {UserSchema} = require('../model/user-model')
 
 const User = mongoose.model('user', UserSchema)
 
 exports.signup = async (req, res) => {
 
     if (req.body.name === undefined) {
-        return res.status(400).send({message: 'Field name is required!'});
+        return res.status(400).send({message: 'Field name is required!'})
     }
     if (req.body.email === undefined) {
-        return res.status(400).send({message: 'Field email is required!'});
+        return res.status(400).send({message: 'Field email is required!'})
     }
     if (req.body.password === undefined) {
-        return res.status(400).send({message: 'Field password is required!'});
+        return res.status(400).send({message: 'Field password is required!'})
     }
 
     bcrypt.hash(req.body.password, 10, async (errBcrypt, hash) => {
         if (errBcrypt) {
-            return res.status(500).send({error: errBcrypt});
+            return res.status(500).send({error: errBcrypt})
         }
 
         const new_user = new User({
@@ -30,12 +31,12 @@ exports.signup = async (req, res) => {
         const user = await User.findOne({email: new_user.email})
 
         if (user !== null) {
-            return res.status(409).send({message: 'E-mail already registered'});
+            return res.status(409).send({message: 'E-mail already registered'})
         }
 
         new_user.save((err) => {
             if (err) {
-                return res.status(500).send(err);
+                return res.status(500).send(err)
             }
 
             return res.status(201).send({
@@ -45,7 +46,38 @@ exports.signup = async (req, res) => {
                     email: new_user.email,
                     created_date: new_user.created_date,
                 }
-            });
+            })
+        })
+    })
+}
+
+exports.login = async (req, res) => {
+
+    const user = await User.findOne({email: req.body.email})
+
+    bcrypt.compare(req.body.password, user.password, (bError, bResult) => {
+        if (bError) {
+            return res.status(401).send({message: 'Auth failed'})
+        }
+
+        if (!bResult) {
+            return res.status(401).send({message: 'Password mismatch'})
+        }
+
+        const token = jsonwebtoken.sign({
+                _id: user._id,
+                email: user.email,
+                name: user.name
+            },
+            process.env.JWT_KEY,
+            {
+                expiresIn: process.env.JWT_EXPIRES
+            })
+
+        return res.status(200).send({
+            name: user.name,
+            email: user.email,
+            token: token,
         })
     })
 }
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const UserController = require('../controllers/user-controller');
 
 router.post('/signup', UserController.signup);
+router.post('/login', UserController.login);
 
 module.exports = router;
